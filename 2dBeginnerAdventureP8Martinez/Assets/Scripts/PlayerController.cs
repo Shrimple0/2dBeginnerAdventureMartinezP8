0e3521a084efa3f4ad22118410f0dcbc7be4b6e6
Shrimple0 <756116@student.dusd.net>
2024-11-07 15:23:46 -0800

    started animation work

--- 2dBeginnerAdventureP8Martinez/Assets/Scripts/PlayerController.cs
+++ 2dBeginnerAdventureP8Martinez/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float speed = 150f;
+    public float speed = 50f;
     public int maxHealth = 5;
     public int health { get { return currentHealth; } }
     int currentHealth;
@@ -13,11 +13,15 @@ public class NewBehaviourScript : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
       rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
     }
@@ -28,6 +32,21 @@ public class NewBehaviourScript : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+     
+    }
+        
+        void FixedUpdate()
+        { 
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
@@ -36,6 +55,7 @@ public class NewBehaviourScript : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        animator.SetTrigger("Hit");
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
